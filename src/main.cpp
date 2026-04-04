711e0e2b75b5e20dac026f6f6acc51a14c913b0b
wom <izik.pfirrmann@gmail.com>
2026-04-04 12:03:25 -0500

    just making things compile, broke the clean and cc.json function

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #include <cstring>
 #include <filesystem>
 #include <format>
-#include <numeric>
 #include <string_view>
 #include <thread>
 #include <type_traits>
@@ -777,6 +776,9 @@ static auto build(lua_State *const state) noexcept -> exit_t {
 // TODO: there's some error here where if you have multipe modules it will only
 // clean one of them(?), not sure how to fix it :)
 static auto clean(lua_State *const state) noexcept -> exit_t {
+  error_message(
+      "clean function not implimented, need to fix how modules are stored");
+  return exit_t::internal_error;
   auto const build_fn_t = lua_getglobal(state, "Build");
   switch (build_fn_t) {
   case LUA_TFUNCTION:
@@ -806,15 +808,20 @@ static auto clean(lua_State *const state) noexcept -> exit_t {
     return exit_t::lua_vm_error; // ?
   }
 
+#if 0
   for (auto &&mod : builtins::mods) {
     auto const cache_path = fs::path(
         std::format("{}/__luamake_cache/{}.cache", mod.install_dir, mod.name));
     (void)fs::remove(cache_path);
   }
+#endif
   return exit_t::ok;
 }
 
 static auto compile_commands_json(lua_State *const state) noexcept -> exit_t {
+  error_message("Generating compile_command.json is not currently implimented, "
+                "fixing how modules are stored and accessed");
+  return exit_t::internal_error;
   auto const build_fn_t = lua_getglobal(state, "Build");
   switch (build_fn_t) {
   case LUA_TFUNCTION:
@@ -844,6 +851,7 @@ static auto compile_commands_json(lua_State *const state) noexcept -> exit_t {
     return exit_t::lua_vm_error; // ?
   }
 
+#if 0
   for (auto &&mod : builtins::mods) {
     auto const &directory = mod.install_dir;
     auto const arguments = [&]() -> string {
@@ -945,6 +953,7 @@ static auto compile_commands_json(lua_State *const state) noexcept -> exit_t {
     }
     cc_json.write(cc_json_string.c_str(), cc_json_string.size(), 1);
   }
+#endif
   return exit_t::ok;
 }
 
