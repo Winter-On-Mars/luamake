a70128a57af9f724a8700af6aab45fb22fbf0261
wom <izik.pfirrmann@gmail.com>
2026-04-23 10:18:17 -0500

    removed note because it *should* be resolved

--- src/luamake_builtins.hpp
+++ src/luamake_builtins.hpp
@@ -105,15 +105,6 @@ struct Module final {
   // without having to worry about pointer indirection
   // TODO: have the DepTree depend on the LakeModules, where all of these paths
   // are relative to said lakemodule
-  // NOTE: there is some bug going on right now where when we have the following
-  // set up,
-  // m.cpp < a.hpp, b.hpp
-  // a.hpp
-  // b.hpp
-  // a.cpp
-  // b.cpp < a.hpp
-  // where < indicateds that LHS depends on RHS
-  // i assume somewhere we are returning a {0,0} somewhere, causing the issue
   struct DepTree final {
     enum class SourceFile_t : u8 {
       IMPL,
