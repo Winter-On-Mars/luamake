46510278cda575a479e8b9be83592896a4b16380
wom <izik.pfirrmann@gmail.com>
2026-05-01 19:57:16 -0500

    added gcc function because i forgot

--- src/luamake_builtins.hpp
+++ src/luamake_builtins.hpp
@@ -44,8 +44,10 @@ class Builder final {
   static auto install_static(lua_State *) noexcept -> int;
 
   static auto clang(lua_State *) noexcept -> int;
+  static auto gcc(lua_State *) noexcept -> int;
   static auto gcc_bare(lua_State *) noexcept -> int;
   static auto clang_bare(lua_State *) noexcept -> int;
+
   static auto require(lua_State *) noexcept -> int;
   static auto link_lib(lua_State *) noexcept -> int;
 
